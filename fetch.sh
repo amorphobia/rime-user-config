c3fe140d82eb4813c79dc8bb0415c68a9903b5ba
Xuesong Peng <pengxuesong.cn@gmail.com>
2023-05-05 14:26:52 +0800

    update to latest rime-emoji

--- fetch.sh
+++ fetch.sh
@@ -13,10 +13,11 @@ rm -rf jiandao && \
 git clone --depth 1 https://github.com/xkinput/Rime_JD jiandao --branch plum && \
 cp jiandao/xkjd6.*.yaml ${SCHEMAS}/ && \
 cp jiandao/lua/* ${SCHEMAS}/lua/ && \
-cp jiandao/opencc/* ${SCHEMAS}/opencc/ && \
+cp jiandao/opencc/EN2en* ${SCHEMAS}/opencc/ && \
 cp jiandao/rime.lua ${SCHEMAS}/
 
 # 🍀四叶草
+# https://github.com/fkxxyz/rime-cloverpinyin
 clover_ver="1.1.4"
 clover_zip="clover.schema-${clover_ver}.zip"
 clover_url="https://github.com/fkxxyz/rime-cloverpinyin/releases/download/${clover_ver}/${clover_zip}"
@@ -27,7 +28,7 @@ rm -rf clover && mkdir -p clover && (
     rm -rf ${clover_zip}
 ) && \
 cp clover/*.yaml ${SCHEMAS}/ && \
-cp clover/opencc/* ${SCHEMAS}/opencc/
+cp clover/opencc/symbol* ${SCHEMAS}/opencc/
 
 # 🇯🇵日本語
 # https://github.com/gkovacs/rime-japanese
@@ -41,3 +42,14 @@ rm -rf shupin && \
 git clone --depth 1 https://github.com/Papnas/shupin && \
 cp shupin/*.dict.yaml ${SCHEMAS}/ && \
 cp shupin/shupin_congqin.schema.yaml ${SCHEMAS}/
+
+# 😂绘文字
+# https://github.com/rime/rime-emoji
+if command -v opencc &> /dev/null
+then
+    rm -rf emoji
+    git clone --depth 1 https://github.com/rime/rime-emoji emoji
+    opencc -c t2s.json -i emoji_category.txt -o emoji_category.txt
+    opencc -c t2s.json -i emoji_word.txt -o emoji_word.txt
+    cp emoji/opencc/* ${SCHEMAS}/opencc/
+fi
