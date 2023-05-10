6cd2044748cf9064a38588c76e09a0c1ce8b8790
Xuesong Peng <pengxuesong.cn@gmail.com>
2023-05-10 15:56:57 +0800

    fix file path

--- fetch.sh
+++ fetch.sh
@@ -12,13 +12,15 @@ mkdir -p ${SCHEMAS}/opencc
 # ⭐星空键道
 # https://github.com/xkinput/Rime_JD
 rm -rf jiandao && \
-git clone --depth 1 https://github.com/xkinput/Rime_JD jiandao --branch plum && \
-for dict in danzi cizu fuhao buchong lianjie chaojizici wxw
-do
-    sed '0,/^\.\.\.$/d' jiandao/xkjd6.${dict}.dict.yaml >> jiandao/xkjd6.dict.yaml && \
-    rm jiandao/xkjd6.${dict}.dict.yaml
-done && \
-rm jiandao/xkjd6.yingwen.dict.yaml jiandao/xkjd6.extended.dict.yaml && \
+git clone --depth 1 https://github.com/xkinput/Rime_JD jiandao --branch plum && (
+    cd jiandao && \
+    for dict in danzi cizu fuhao buchong lianjie chaojizici wxw
+    do
+        sed '0,/^\.\.\.$/d' xkjd6.${dict}.dict.yaml >> xkjd6.dict.yaml && \
+        rm xkjd6.${dict}.dict.yaml
+    done && \
+    rm xkjd6.yingwen.dict.yaml xkjd6.extended.dict.yaml
+) && \
 cp jiandao/xkjd6.*.yaml ${SCHEMAS}/ && \
 cp jiandao/lua/* ${SCHEMAS}/lua/ && \
 cp jiandao/rime.lua ${SCHEMAS}/ && \
@@ -30,18 +32,18 @@ clover_ver="1.1.4"
 clover_zip="clover.schema-${clover_ver}.zip"
 clover_url="https://github.com/fkxxyz/rime-cloverpinyin/releases/download/${clover_ver}/${clover_zip}"
 rm -rf clover && mkdir -p clover && (
-    cd clover
-    curl -LO "${clover_url}"
-    unzip "${clover_zip}" -d .
-    rm -rf ${clover_zip}
+    cd clover && \
+    curl -LO "${clover_url}" && \
+    unzip "${clover_zip}" -d . && \
+    rm -rf ${clover_zip} && \
+    sed -i -n '/import_tables/q;p' clover.dict.yaml && \
+    echo '...' >> clover.dict.yaml && \
+    for dict in clover.base clover.phrase THUOCL_animal THUOCL_caijing THUOCL_car THUOCL_chengyu THUOCL_diming THUOCL_food THUOCL_IT THUOCL_law THUOCL_lishimingren THUOCL_medical THUOCL_poem sogou_new_words
+    do
+        sed '0,/^\.\.\.$/d' ${dict}.dict.yaml >> clover.dict.yaml && \
+        rm ${dict}.dict.yaml
+    done
 ) && \
-sed -i -n '/import_tables/q;p' clover/clover.dict.yaml && \
-echo '...' >> clover/clover.dict.yaml && \
-for dict in clover.base clover.phrase THUOCL_animal THUOCL_caijing THUOCL_car THUOCL_chengyu THUOCL_diming THUOCL_food THUOCL_IT THUOCL_law THUOCL_lishimingren THUOCL_medical THUOCL_poem sogou_new_words
-do
-    sed '0,/^\.\.\.$/d' clover/${dict}.dict.yaml >> clover/clover.dict.yaml && \
-    rm clover/${dict}.dict.yaml
-done && \
 cp clover/*.yaml ${SCHEMAS}/ && \
 cp clover/opencc/symbol* ${SCHEMAS}/opencc/ && \
 echo "fetch clover done."
@@ -49,13 +51,14 @@ echo "fetch clover done."
 # 🇯🇵日本語
 # https://github.com/gkovacs/rime-japanese
 rm -rf japanese && \
-git clone --depth 1 https://github.com/gkovacs/rime-japanese japanese && \
-sed -i '/import_tables/,/^\.\.\.$/{/^\./!d}' japanese.dict.yaml && \
-for dict in mozc jmdict kana
-do
-    sed '0,/^\.\.\.$/d' japanese/japanese.${dict}.dict.yaml >> japanese.dict.yaml && \
-    rm japanese/japanese.${dict}.dict.yaml
-done && \
+git clone --depth 1 https://github.com/gkovacs/rime-japanese japanese && (
+    sed -i '/import_tables/,/^\.\.\.$/{/^\./!d}' japanese.dict.yaml && \
+    for dict in mozc jmdict kana
+    do
+        sed '0,/^\.\.\.$/d' japanese.${dict}.dict.yaml >> japanese.dict.yaml && \
+        rm japanese.${dict}.dict.yaml
+    done
+) && \
 cp japanese/*.yaml ${SCHEMAS}/ && \
 echo "fetch japanese done."
 
