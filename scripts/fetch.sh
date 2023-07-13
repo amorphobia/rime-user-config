b3fffc78e6d35d6e3f8d67f28ccc9fe610765fc7
Xuesong Peng <pengxuesong.cn@gmail.com>
2023-07-13 12:05:20 +0800

    chore(ci): add more sanity checks
    move scripts under a dedicate directory

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 # encoding: utf-8
+
+# Fetch Schemas
+# Copyright (C) 2023  Fu Xiao <https://github.com/imfuxiao>
+# Copyright (C) 2023  Xuesong Peng <pengxuesong.cn@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Original idea & script (GPL-3.0 license) can be found at
+# https://github.com/imfuxiao/HamsterInputSchemas/blob/520ff76aada26b3c05851ba6feb42c72a5558021/build.sh
+
 set -e
 
 VERSION="master"
---
+++ scripts/sanity_check.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Sanity Check
+# Copyright (C) 2023  Xuesong Peng <pengxuesong.cn@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+WORK=$(pwd)
+
+sort -k2,2 -k4,4nr -k5,5nr -k3,3 dicts/cizu_append.txt | awk '!seen[$1,$2]++' > dicts/cizu_append_processed.txt
+res=$(diff dicts/cizu_append.txt dicts/cizu_append_processed.txt)
+
+if [[ $res ]]; then
+    exit 1
+fi
+
+sort -k2,2 -s dicts/cizu_delete.txt | awk '!seen[$1,$2]++' > dicts/cizu_delete_processed.txt
+res=$(diff dicts/cizu_delete.txt dicts/cizu_delete_processed.txt)
+
+if [[ $res ]]; then
+    exit 1
+fi
+
+sort -k2,2 -s dicts/cizu_modify.txt | awk '!seen[$1,$2]++' > dicts/cizu_modify_processed.txt
+res=$(diff dicts/cizu_modify.txt dicts/cizu_modify_processed.txt)
+
+if [[ $res ]]; then
+    exit 1
+fi
+
+exit 0
